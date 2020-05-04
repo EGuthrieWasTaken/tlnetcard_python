fb4b07eccf2242aa344712f8eaff83dc9a937277
EGuthrieWasTaken <guthrieec@cofc.edu>
2020-05-04 11:17:54 -0400

    Add README.md and update console.py

--- tlnetcard_python/system/administration/console/console.py
+++ tlnetcard_python/system/administration/console/console.py
@@ -5,5 +5,9 @@
 
 class Console:
     """ Class for the Console object. """
-    def __init__(self):
+    def __init__(self, login_object):
         """ Initializes the Console object. """
+        self._login_object = login_object
+        self._get_url = login_object.get_base_url() + "/en/adm_console.asp"
+        self._post_url = login_object.get_base_url() + "/delta/adm_console"
+    
