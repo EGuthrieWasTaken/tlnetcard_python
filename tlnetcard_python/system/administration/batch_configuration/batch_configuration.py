a27c1f3dfa7e2f8320434dd93373d6dee80e6766
EGuthrieWasTaken <guthrieec@cofc.edu>
2020-05-12 12:13:59 -0400

    Still fixing upload methods in batch_configuration.py

--- tlnetcard_python/system/administration/batch_configuration/batch_configuration.py
+++ tlnetcard_python/system/administration/batch_configuration/batch_configuration.py
@@ -65,14 +65,12 @@ class BatchConfiguration:
 
         # Creating upload payload.
         upload_data = {
+            'UL_F_SNMP': open(path, 'rb'),
             'UL_SNMP': 'Upload'
-        }
-        upload_file = {
-            'UL_F_SNMP': open(path, 'rb')
         }
 
         # Uploading SNMP configuration.
-        self._login_object.get_session().post(self._post_url, data=upload_data, files=upload_file,
+        self._login_object.get_session().post(self._post_url, data=upload_data,
                                               verify=self._login_object.get_reject_invalid_certs())
         print("NOTE: The card at " + self._login_object.get_base_url()
               + " will be offline for approximately 10 seconds.")
@@ -87,14 +85,12 @@ class BatchConfiguration:
 
         # Creating upload payload.
         upload_data = {
+            'UL_F_SNMP': open(path, 'rb'),
             'UL_SNMP': 'Upload'
-        }
-        upload_file = {
-            'UL_F_SNMP': open(path, 'rb')
         }
 
         # Uploading system configuration.
-        self._login_object.get_session().post(self._post_url, data=upload_data, files=upload_file,
+        self._login_object.get_session().post(self._post_url, data=upload_data,
                                               verify=self._login_object.get_reject_invalid_certs())
         print("NOTE: The card at " + self._login_object.get_base_url()
               + " will be offline for approximately 10 seconds.")
