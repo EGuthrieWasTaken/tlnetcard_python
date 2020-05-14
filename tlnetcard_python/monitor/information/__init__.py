35bbe2d28840c5070664a7f8c5a7ec0e8c380fc2
EGuthrieWasTaken <guthrieec@cofc.edu>
2020-05-14 13:40:05 -0400

    Initialize monitor.information subclasses

---
+++ tlnetcard_python/monitor/information/__init__.py
@@ -0,0 +1,7 @@
+""" Initializes Information subclasses. """
+from tlnetcard_python.monitor.information.ups_properties import UpsProperties
+from tlnetcard_python.monitor.information.battery_parameters import BatteryParameters
+from tlnetcard_python.monitor.information.in_out_parameters import InOutParameters
+from tlnetcard_python.monitor.information.identification import Identification
+from tlnetcard_python.monitor.information.status_indication import StatusIndication
+from tlnetcard_python.monitor.information.shutdown_agent import ShutdownAgent
---
+++ tlnetcard_python/monitor/information/battery_parameters/battery_parameters.py
@@ -0,0 +1,9 @@
+# battery_parameters.py
+# Ethan Guthrie
+# 05/14/2020
+""" Allows battery parameters to be read. """
+
+class Battery_Parameters:
+    """ Class for the Battery_Parameters object. """
+    def __init__(self, login_object):
+        """ Initializes the Battery_Parameters object. """
