f9d75d2eff310eb79130f9b17017f62246a84469
EGuthrieWasTaken <guthrieec@cofc.edu>
2020-06-03 13:59:57 -0400

    Update in-line documenation in information.py

--- tlnetcard_python/monitor/about/information/information.py
+++ tlnetcard_python/monitor/about/information/information.py
@@ -1,20 +1,49 @@
-# information.py
-# Ethan Guthrie
-# 05/13/2020
-""" Allows the firmware version for the TLNETCARD to be retreived. """
+"""
+tlnetcard_python.monitor.about.information.information
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module provides an ``Information`` object to allow the TLNET Supervisor firmware version to be
+retreived.
+"""
 
 # Required internal classes/functions.
 from tlnetcard_python.login import Login
 
 # pylint: disable=too-few-public-methods
 class Information:
-    """ Class for the Information object. """
+    """
+    A TLNET Supervisor ``Information`` object. Provides the functionality of the equivalent webpage
+    TLNET Supervisor -> Monitor -> About -> Information.
+
+    Basic Usage:
+
+    >>> from tlnetcard_python import Login
+    >>> from tlnetcard_python.monitor.about import Information
+    >>> # As always, a tlnetcard_python.Login object must first be created. Then the Login object
+    >>> # can be passed to the tlnetcard_python.monitor.about.Information object.
+    >>> card = Login(user="admin", passwd="password", host="10.0.0.100")
+    >>> card_info = Information(card)
+    >>> # Now that the Information object has been created, functions belonging to the Information
+    >>> # class can be used. For example GETing the current firmware version (the only function in
+    >>> # this class):
+    >>> card_info.get_firmware_version()
+    '01.12.05c'
+    """
     def __init__(self, login_object: Login) -> None:
-        """ Initializes the BatchConfiguration object. """
+        """
+        Initializes the ``BatchConfiguration`` object. Returns ``None``.
+
+        :param login_object: A valid ``tlnetcard_python.Login`` object.
+        :rtype: ``None``
+        """
         self._login_object = login_object
         self._get_url = login_object.get_base_url() + "/en/ups/about_info.asp"
     def get_firmware_version(self) -> str:
-        """ GETs the current firmware version. """
+        """
+        Returns the current TLNET Supervisor firmware version as a string.
+
+        :rtype: ``str``
+        """
         # GETing Information page.
         verify = self._login_object.get_reject_invalid_certs()
         resp = self._login_object.get_session().get(self._get_url,
