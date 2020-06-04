750823114a5d12063fc49e65213c1261b27e8ad3
EGuthrieWasTaken <guthrieec@cofc.edu>
2020-06-04 14:48:26 -0400

    Update in-line documentation for syslog.py

--- tlnetcard_python/system/administration/syslog/syslog.py
+++ tlnetcard_python/system/administration/syslog/syslog.py
@@ -1,4 +1,10 @@
-""" Allows syslog servers to be added or removed. """
+"""
+tlnetcard_python.system.administration.syslog.syslog
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module provides a ``Syslog`` object to provide the functionality of TLNET Supervisor -> System
+-> Administration -> Syslog.
+"""
 
 # Standard library.
 from typing import List
@@ -6,14 +12,42 @@ from typing import List
 from tlnetcard_python.login import Login
 
 class Syslog:
-    """ Class for the syslog object. """
+    """
+    A TLNET Supervisor ``Syslog`` object. Provides the functionality of the equivalent webpage
+    TLNET Supervisor -> System -> Administration -> Syslog.
+
+    Basic Usage:
+
+    >>> from tlnetcard_python import Login
+    >>> from tlnetcard_python.system.administration import Syslog
+    >>> # As always, a tlnetcard_python.Login object must first be created. Then the Login object
+    >>> # can be passed to the tlnetcard_python.system.administration.Syslog object.
+    >>> card = Login(user="admin", passwd="password", host="10.0.0.100")
+    >>> card_syslog = Syslog(card)
+    >>> # Now that the Syslog object has been created, functions belonging to the Syslog class can
+    >>> # be used. For example, adding a new syslog server:
+    >>> card_syslog.add_server("10.0.0.200")
+    True
+    """
     def __init__(self, login_object: Login) -> None:
-        """ Initializes the Syslog object. """
+        """
+        Initializes the ``Syslog`` object. Returns ``None``.
+
+        :param login_object: A valid ``tlnetcard_python.Login`` object.
+        :rtype: ``None``
+        """
         self._login_object = login_object
         self._get_url = login_object.get_base_url() + "/en/adm_syslog.asp"
         self._post_url = login_object.get_base_url() + "/delta/adm_syslog"
     def add_server(self, server: str) -> bool:
-        """ Adds a syslog server. """
+        """
+        Adds a syslog server. If there are already more than 4 syslog servers in use, the provided
+        server will not be added and ``False`` will be returned. Otherwise, ``True`` will be
+        returned upon success.
+
+        :param server: The address of the server to add.
+        :rtype: ``bool``
+        """
         # Quitting if four servers are already listed.
         curr_servers = self.get_servers()
         if len(curr_servers) >= 4:
@@ -45,7 +79,11 @@ class Syslog:
         self._login_object.request_system_config_renewal()
         return True
     def clear_servers(self) -> None:
-        """ Clears all syslog servers. """
+        """
+        Clears all syslog servers. Returns ``None``.
+
+        :rtype: ``None``
+        """
         # Generating payload.
         syslog_data = {}
         for i in range(0, 4):
@@ -58,7 +96,11 @@ class Syslog:
                                               ).raise_for_status()
         self._login_object.request_system_config_renewal()
     def disable_syslog(self) -> None:
-        """ Disables syslog servers. """
+        """
+        Disables syslog. Returns ``None``.
+
+        :rtype: ``None``
+        """
         # Generating payload.
         syslog_data = {
             'SLG_SLG': 0
@@ -71,7 +113,11 @@ class Syslog:
                                               ).raise_for_status()
         self._login_object.request_system_config_renewal()
     def enable_syslog(self) -> None:
-        """ Enables syslog servers. """
+        """
+        Enables syslog. Returns ``None``.
+
+        :rtype: ``None``
+        """
         # Generating payload.
         syslog_data = {
             'SLG_SLG': 1
@@ -84,7 +130,11 @@ class Syslog:
                                               ).raise_for_status()
         self._login_object.request_system_config_renewal()
     def get_servers(self) -> List[str]:
-        """ GETs syslog servers and returns them in a list. """
+        """
+        Returns syslog servers in a list.
+
+        :rtype: ``List[str]``
+        """
         # GETing system config.
         system_config = self._login_object.get_system_config()
         items = ["SysLog Server", "SysLog Server2", "SysLog Server3", "SysLog Server4"]
@@ -95,7 +145,13 @@ class Syslog:
                 servers.append(system_config[i])
         return servers
     def remove_server(self, server: str) -> bool:
-        """ Removes a syslog server. """
+        """
+        Removes a syslog server. If the provided server is not listed, ``False`` is returned.
+        Otherwise, ``True`` will be returned upon success.
+
+        :param server: The server to be removed.
+        :rtype: ``bool``
+        """
         # Quitting if server isn't listed.
         curr_servers = self.get_servers()
         if server not in curr_servers:
